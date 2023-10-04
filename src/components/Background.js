f8239d748d7fadf75e41e98610e93b8f423c647a
Peter Lin <silentvow@gmail.com>
2023-10-04 18:01:32 +0800

    fix: bg

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,14 +1,19 @@
-import { IMG_URLS, SCREEN_WIDTH } from '@/constants/game'
+import { IMG_URLS, SCREEN_HEIGHT, SCREEN_WIDTH } from '@/constants/game'
 import { TilingSprite } from '@pixi/react'
+
+const LINE_HEIGHT = 64
+const LAYER_1_HEIGHT = 256
+const LAYER_3_HEIGHT = 256
+const LAYER_2_HEIGHT = SCREEN_HEIGHT - LAYER_3_HEIGHT - LAYER_1_HEIGHT - LINE_HEIGHT * 2
 
 function Background () {
   return (
     <>
-      <TilingSprite x={0} y={0} width={SCREEN_WIDTH} height={256} image={IMG_URLS.BACKGROUND_1} />
-      <TilingSprite x={0} y={256} width={SCREEN_WIDTH} height={16} image={IMG_URLS.BACKGROUND_2} />
-      <TilingSprite x={0} y={272} width={SCREEN_WIDTH} height={496 - 272} image={IMG_URLS.BACKGROUND_3} />
-      <TilingSprite x={0} y={496} width={SCREEN_WIDTH} height={16} image={IMG_URLS.BACKGROUND_4} />
-      <TilingSprite x={0} y={512} width={SCREEN_WIDTH} height={448} image={IMG_URLS.BACKGROUND_5} />
+      <TilingSprite x={0} y={0} width={SCREEN_WIDTH} height={LAYER_1_HEIGHT} image={IMG_URLS.BACKGROUND_1} />
+      <TilingSprite x={0} y={LAYER_1_HEIGHT} width={SCREEN_WIDTH} height={LINE_HEIGHT} image={IMG_URLS.BACKGROUND_2} />
+      <TilingSprite x={0} y={LAYER_1_HEIGHT + LINE_HEIGHT} width={SCREEN_WIDTH} height={LAYER_2_HEIGHT} image={IMG_URLS.BACKGROUND_3} />
+      <TilingSprite x={0} y={SCREEN_HEIGHT - LAYER_3_HEIGHT - LINE_HEIGHT} width={SCREEN_WIDTH} height={LINE_HEIGHT} image={IMG_URLS.BACKGROUND_4} />
+      <TilingSprite x={0} y={SCREEN_HEIGHT - LAYER_3_HEIGHT} width={SCREEN_WIDTH} height={LAYER_3_HEIGHT} image={IMG_URLS.BACKGROUND_5} />
     </>
   )
 }
