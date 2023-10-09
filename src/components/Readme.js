e7266990cb6b1dcd6ed3577b95f8325165db9a99
Peter Lin <silentvow@gmail.com>
2023-10-09 14:50:29 +0800

    feat: update readme

--- src/components/Readme.js
+++ src/components/Readme.js
@@ -8,21 +8,23 @@ import MenuButton from './MenuButton'
 const INTRODUCE_TEXT = `遊戲規則：
 用滑鼠移動灰妲反彈咪姆或者點擊左鍵發射火球以消除
 所有方塊，獲取掉落的金錢，避免咪姆掉落至灰妲下方。
-
+                                點擊Ｐ鍵暫停遊戲。
 方塊掉落物：
-  增加火球的發射次數      復原咪姆的攻擊力
   增加咪姆的攻擊力        減少咪姆的攻擊力
+  使咪姆數量變成兩倍      復原咪姆的攻擊力
   放大咪姆的尺寸          縮小咪姆的尺寸
   降低咪姆的移動速度      提高咪姆的移動速度
   使灰妲變大              使灰妲變小
-  使咪姆數量變成兩倍      隨機一種其他物品效果
+  增加火球的發射次數      隨機一種其他物品效果
   獲得 $${MONEY_VALUES[ITEM.MONEY_XL]}            獲得 $${MONEY_VALUES[ITEM.MONEY_LG]}
   獲得 $${MONEY_VALUES[ITEM.MONEY_MD]}             獲得 $${MONEY_VALUES[ITEM.MONEY_SM]}
   獲得 $${MONEY_VALUES[ITEM.MONEY_XS]}
 `
 
 const STAFF_TEXT = `製作：彼得與狼
-美術：黑櫻焦阿巴`
+美術：黑櫻焦阿巴
+素材來源：ReLive_灰妲 DaDa、ReLive_咪姆 MeeMu、
+          いらすとや、Rotting Pixels`
 
 const FONT_SIZE = 45
 const LINE_HEIGHT = FONT_SIZE * 1.2
@@ -34,6 +36,18 @@ const textStyle = new TextStyle({
   fill: '#ffffff',
   strokeThickness: 5,
   lineHeight: LINE_HEIGHT,
+  breakWords: true,
+  wordWrap: true,
+  wordWrapWidth: 1280 - 80 - 32,
+})
+
+const captionStyle = new TextStyle({
+  align: 'left',
+  fontFamily: 'Roboto, "Xiaolai Mono SC", sans-serif',
+  fontSize: 24,
+  fill: '#ffffff',
+  strokeThickness: 5,
+  lineHeight: 24 * 1.2,
   breakWords: true,
   wordWrap: true,
   wordWrapWidth: 1280 - 80 - 32,
@@ -66,7 +80,7 @@ function Readme () {
       <Sprite x={40 + 16} y={20 + 16 + (LINE_HEIGHT - ITEM_HEIGHT) * 0.5 + LINE_HEIGHT * 12} image={IMG_URLS[ITEM.MONEY_MD]} />
       <Sprite x={40 + 16 + 540} y={20 + 16 + (LINE_HEIGHT - ITEM_HEIGHT) * 0.5 + LINE_HEIGHT * 12} image={IMG_URLS[ITEM.MONEY_SM]} />
       <Sprite x={40 + 16} y={20 + 16 + (LINE_HEIGHT - ITEM_HEIGHT) * 0.5 + LINE_HEIGHT * 13} image={IMG_URLS[ITEM.MONEY_XS]} />
-      <Text x={40 + 16} y={960 - 20 - 16} anchor={[0, 1]} text={STAFF_TEXT} style={textStyle} />
+      <Text x={40 + 16} y={960 - 20 - 16} anchor={[0, 1]} text={STAFF_TEXT} style={captionStyle} />
       <MenuButton x={760} y={140 + 695} text='返回前頁' onClick={enterMainMenu} />
     </Container>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App ({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>灰妲x咪姆x發大財</title>
+        <title>灰妲x咪姆x秘境尋寶</title>
       </Head>
       <Component {...pageProps} />
     </>
