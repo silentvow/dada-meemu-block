07b0d877d47b72b03a9fc050ab5781af88a43745
Peter Lin <silentvow@gmail.com>
2023-10-10 19:30:42 +0800

    feat: favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,11 +2,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document () {
   return (
-    <Html lang='en'>
+    <Html lang='zh-Hant'>
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        {/* <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Sono&display=swap' rel='stylesheet' /> */}
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
       <body>
         <Main />
