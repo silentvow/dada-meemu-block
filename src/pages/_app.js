5688096b2c2190c243ca09c9b1aaf460ab1964dd
Peter Lin <silentvow@gmail.com>
2023-10-11 19:45:42 +0800

    fix: description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,8 @@ import { useRouter } from 'next/router'
 import Script from 'next/script'
 import { useEffect } from 'react'
 
+const description = '《灰妲x咪姆x遺跡尋寶》是一款快節奏的打磚塊同人二次創作遊戲。Vtuber灰妲與咪姆將進入秘密的藏寶處，突破棘手的關卡，搜刮掉落的金錢。\n你需要運用技巧，操控球體，摧毀各種磚塊，遊戲中有多樣的道具，可以提高你的分數。挑戰你的朋友，爭取最高分，成為最優秀的寶藏獵人。'
+
 export default function App ({ Component, pageProps }) {
   const router = useRouter()
   useEffect(() => {
@@ -28,14 +30,14 @@ export default function App ({ Component, pageProps }) {
     <>
       <Head>
         <title>【同人遊戲】灰妲x咪姆x遺跡尋寶</title>
-        <meta name='description' content='意外獲得藏寶圖的灰妲，與被拉壯丁的咪姆開始一場探險。' />
+        <meta name='description' content={description} />
         <meta property='og:url' content='https://dada-meemu-block.vercel.app' />
         <meta property='og:locale' content='zh_TW' />
         <meta property='og:type' content='website' />
         <meta property='og:title' content='【同人遊戲】灰妲x咪姆x遺跡尋寶' />
-        <meta property='og:description' content='意外獲得藏寶圖的灰妲，與被拉壯丁的咪姆開始一場探險。' />
+        <meta property='og:description' content={description} />
         <meta property='og:image' content='https://dada-meemu-block.vercel.app/img/og.png' />
-        <meta property='og:image:alt' content='意外獲得藏寶圖的灰妲，與被拉壯丁的咪姆開始一場探險。' />
+        <meta property='og:image:alt' content={description} />
         <meta property='og:image:type' content='image/png' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
@@ -43,8 +45,8 @@ export default function App ({ Component, pageProps }) {
         <meta name='twitter:title' content='【同人遊戲】灰妲x咪姆x遺跡尋寶' />
         <meta name='twitter:site' content='@tn604000' />
         <meta name='twitter:image' content='https://dada-meemu-block.vercel.app/img/x.png' />
-        <meta name='twitter:image:alt' content='意外獲得藏寶圖的灰妲，與被拉壯丁的咪姆開始一場探險。' />
-        <meta name='twitter:description' content='意外獲得藏寶圖的灰妲，與被拉壯丁的咪姆開始一場探險。' />
+        <meta name='twitter:image:alt' content={description} />
+        <meta name='twitter:description' content={description} />
         <meta name='twitter:creator' content='@tn604000' />
         <meta property='twitter:domain' content='dada-meemu-block.vercel.app' />
         <meta property='twitter:url' content='https://dada-meemu-block.vercel.app' />
