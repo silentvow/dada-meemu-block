9faa55934c412edc93df3caeee26f7717e354b6d
Peter Lin <silentvow@gmail.com>
2023-10-24 00:37:05 +0800

    docs: changelog mandarin

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,98 +1,104 @@
 import Header from '@/components/Header'
+import { useTranslations } from 'next-intl'
 
 function Changelog () {
+  const t = useTranslations('changelog')
   return (
     <>
       <Header />
       <div className='flex-col flex'>
         <div className='p-8 flex justify-center'>
           <div className='w-[1280px]'>
+            <h2 className='text-2xl border-b border-b-foreground'>2023-10-23</h2>
+            <ul className='list-disc p-6'>
+              <li>{t('2023-10-23-1')}</li>
+            </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-22</h2>
             <ul className='list-disc p-6'>
-              <li>feat: adjust extra story content</li>
+              <li>{t('2023-10-22-1')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-21</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add transition between story scene and game</li>
-              <li>feat: adjust story chapter 1 content</li>
-              <li>feat: adjust fonts</li>
+              <li>{t('2023-10-21-1')}</li>
+              <li>{t('2023-10-21-2')}</li>
+              <li>{t('2023-10-21-3')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-19</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add language switcher</li>
-              <li>fix: window sizing</li>
+              <li>{t('2023-10-19-1')}</li>
+              <li>{t('2023-10-19-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-17</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add suicide function</li>
-              <li>feat: adjust score levels</li>
+              <li>{t('2023-10-17-1')}</li>
+              <li>{t('2023-10-17-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-16</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add changelog page</li>
+              <li>{t('2023-10-16-1')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-15</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add readme and license</li>
-              <li>feat: add about page</li>
+              <li>{t('2023-10-15-1')}</li>
+              <li>{t('2023-10-15-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-14</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add extra story</li>
-              <li>feat: update button design</li>
-              <li>fix: ball collision issues</li>
+              <li>{t('2023-10-14-1')}</li>
+              <li>{t('2023-10-14-2')}</li>
+              <li>{t('2023-10-14-3')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-11</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add pause function</li>
-              <li>feat: add more drop items</li>
-              <li>feat: add analytic libraries</li>
+              <li>{t('2023-10-11-1')}</li>
+              <li>{t('2023-10-11-2')}</li>
+              <li>{t('2023-10-11-3')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-10</h2>
             <ul className='list-disc p-6'>
-              <li>release: deploy test version</li>
-              <li>feat: change images in game</li>
-              <li>feat: record local high score</li>
-              <li>feat: add favicon and meta tags</li>
+              <li>{t('2023-10-10-1')}</li>
+              <li>{t('2023-10-10-2')}</li>
+              <li>{t('2023-10-10-3')}</li>
+              <li>{t('2023-10-10-4')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-09</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add story scripts</li>
-              <li>feat: add game instructions</li>
+              <li>{t('2023-10-09-1')}</li>
+              <li>{t('2023-10-09-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-08</h2>
             <ul className='list-disc p-6'>
-              <li>feat: submit score</li>
-              <li>feat: add ending screen</li>
+              <li>{t('2023-10-08-1')}</li>
+              <li>{t('2023-10-08-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-07</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add main menu</li>
-              <li>feat: add story mode</li>
+              <li>{t('2023-10-07-1')}</li>
+              <li>{t('2023-10-07-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-06</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add loader</li>
-              <li>fix: paddle collision issue</li>
+              <li>{t('2023-10-06-1')}</li>
+              <li>{t('2023-10-06-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-05</h2>
             <ul className='list-disc p-6'>
-              <li>feat: add text</li>
-              <li>fix: ball bounce issue</li>
+              <li>{t('2023-10-05-1')}</li>
+              <li>{t('2023-10-05-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-10-04</h2>
             <ul className='list-disc p-6'>
-              <li>feat: make more stage</li>
-              <li>feat: add game over screen</li>
+              <li>{t('2023-10-04-1')}</li>
+              <li>{t('2023-10-04-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-09-30</h2>
             <ul className='list-disc p-6'>
-              <li>feat: make a simple stage with blocks, ball and paddle</li>
-              <li>feat: make drop items</li>
+              <li>{t('2023-09-30-1')}</li>
+              <li>{t('2023-09-30-2')}</li>
             </ul>
             <h2 className='text-2xl border-b border-b-foreground'>2023-09-29</h2>
             <ul className='list-disc p-6'>
-              <li>feat: create app</li>
+              <li>{t('2023-09-29-1')}</li>
             </ul>
           </div>
         </div>
