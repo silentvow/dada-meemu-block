19681748331cc5fb1980d830c320e9d99015c0f0
Peter Lin <silentvow@gmail.com>
2023-11-01 08:30:20 +0800

    feat: update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Languages, Moon, Sun } from 'lucide-react'
+import { Coffee, Languages, Moon, Sun } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import { useTheme } from 'next-themes'
 import Link from 'next/link'
@@ -14,6 +14,12 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 
@@ -55,12 +61,21 @@ function Header () {
         </nav>
         <div className='ml-auto flex items-center space-x-4'>
           <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='ghost' size='icon'>
-                <Languages className='h-[1.2rem] w-[1.2rem]' />
-                <span className='sr-only'>Toggle language</span>
-              </Button>
-            </DropdownMenuTrigger>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant='ghost' size='icon'>
+                      <Languages className='h-[1.2rem] w-[1.2rem]' />
+                      <span className='sr-only'>Change language</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{t('header.change_language')}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
             <DropdownMenuContent align='end'>
               <DropdownMenuItem onClick={() => switchToLocale('zh')}>
                 中文
@@ -71,13 +86,22 @@ function Header () {
             </DropdownMenuContent>
           </DropdownMenu>
           <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='ghost' size='icon'>
-                <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-                <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-                <span className='sr-only'>Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant='ghost' size='icon'>
+                      <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+                      <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+                      <span className='sr-only'>Change theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{t('header.change_theme')}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
             <DropdownMenuContent align='end'>
               <DropdownMenuItem onClick={() => setTheme('light')}>
                 Light
@@ -90,10 +114,34 @@ function Header () {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+          <Link target='_blank' href='https://www.buymeacoffee.com/peternwolf'>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant='ghost' size='icon'>
+                    <Coffee className='h-[1.2rem] w-[1.2rem]' />
+                    <span className='sr-only'>Buy me a coffee</span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{t('header.buy_me_a_coffee')}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </Link>
           <Link target='_blank' href='https://github.com/silentvow/dada-meemu-block'>
-            <Avatar className='h-8 w-8'>
-              <AvatarImage className='dark:invert' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt='github' />
-            </Avatar>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Avatar className='h-8 w-8'>
+                    <AvatarImage className='dark:invert' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt='github' />
+                  </Avatar>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{t('header.view_github')}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </Link>
         </div>
       </div>
